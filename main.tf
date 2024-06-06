caf5fc9a971361981cf4c32bc18785d4bec5f367
GrigoreAlexandru-Arch <mihaieleandru412@gmail.com>
2024-06-06 13:50:17 +0300

    Added 3 instances for each microservice

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_db_instance" "postgres" {
 }
 
 
-resource "aws_instance" "tpd_1" {
+resource "aws_instance" "tpd_user" {
   ami             = "ami-04b70fa74e45c3917"
   instance_type   = "t2.small"
   key_name        = aws_key_pair.deployer.key_name
@@ -65,6 +65,7 @@ resource "aws_instance" "tpd_1" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
+      "sudo apt upgrade -y",
       "sudo apt-get install -y openjdk-17-jdk",
       "sudo apt-get install -y maven",
       "mkdir -p /home/ubuntu"
@@ -93,11 +94,10 @@ resource "aws_instance" "tpd_1" {
   provisioner "remote-exec" {
     inline = [
       "cd /home/ubuntu/project",
-      "sed -i 's|_POSTGRES_CHANGEME_URL_|${aws_db_instance.postgres.endpoint}|g' TPD_EAR/pom.xml",
-      "sed -i 's|_POSTGRES_CHANGEME_USERNAME_|${var.postgres_username}|g' TPD_EAR/pom.xml",
-      "sed -i 's|_POSTGRES_CHANGEME_PASSWORD_|${var.postgres_password}|g' TPD_EAR/pom.xml",
+      "sed -i 's|_POSTGRES_CHANGEME_URL_|${aws_db_instance.postgres.endpoint}|g' TPD_EAR_USER/pom.xml",
+      "sed -i 's|_POSTGRES_CHANGEME_USERNAME_|${var.postgres_username}|g' TPD_EAR_USER/pom.xml",
+      "sed -i 's|_POSTGRES_CHANGEME_PASSWORD_|${var.postgres_password}|g' TPD_EAR_USER/pom.xml",
       "mvn clean install",
-      "mvn -pl TPD_EAR cargo:start",
     ]
 
     connection {
@@ -109,6 +109,119 @@ resource "aws_instance" "tpd_1" {
   }
 
   tags = {
-    Name = "TPD1"
+    Name = "TPD_USER"
+  }
+}
+
+resource "aws_instance" "tpd_book" {
+  ami             = "ami-04b70fa74e45c3917"
+  instance_type   = "t2.small"
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.all_traffic.name]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt upgrade -y",
+      "sudo apt-get install -y openjdk-17-jdk",
+      "sudo apt-get install -y maven",
+      "mkdir -p /home/ubuntu"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/tpd_aws")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "file" {
+    source      = "~/tpd"
+    destination = "/home/ubuntu/project"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/tpd_aws")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cd /home/ubuntu/project",
+      "sed -i 's|_POSTGRES_CHANGEME_URL_|${aws_db_instance.postgres.endpoint}|g' TPD_EAR_BOOK/pom.xml",
+      "sed -i 's|_POSTGRES_CHANGEME_USERNAME_|${var.postgres_username}|g' TPD_EAR_BOOK/pom.xml",
+      "sed -i 's|_POSTGRES_CHANGEME_PASSWORD_|${var.postgres_password}|g' TPD_EAR_BOOK/pom.xml",
+      "mvn clean install",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/tpd_aws")
+      host        = self.public_ip
+    }
+  }
+
+  tags = {
+    Name = "TPD_BOOK"
+  }
+}
+
+resource "aws_instance" "tpd_web" {
+  ami             = "ami-04b70fa74e45c3917"
+  instance_type   = "t2.small"
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.all_traffic.name]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt upgrade -y",
+      "sudo apt-get install -y openjdk-17-jdk",
+      "sudo apt-get install -y maven",
+      "mkdir -p /home/ubuntu"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/tpd_aws")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "file" {
+    source      = "~/tpd"
+    destination = "/home/ubuntu/project"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/tpd_aws")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cd /home/ubuntu/project",
+      "mvn clean install",
+      "sed -i 's|user_endpoint=localhost:8080|user_endpoint=${aws_instance.tpd_user.public_ip}:8080|g' TPD_WEB/src/main/resources/config.properties",
+      "sed -i 's|book_endpoint=localhost:8080|book_endpoint=${aws_instance.tpd_book.public_ip}:8080|g' TPD_WEB/src/main/resources/config.properties",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/tpd_aws")
+      host        = self.public_ip
+    }
+  }
+
+  tags = {
+    Name = "TPD_WEB"
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,12 +2,10 @@ variable "postgres_username" {
   description = "PostgreSQL database username"
   type        = string
   default     = "postgres"
-  sensitive   = true
 }
 
 variable "postgres_password" {
   description = "PostgreSQL database password"
   type        = string
   default     = "postgres"
-  sensitive   = true
 }
